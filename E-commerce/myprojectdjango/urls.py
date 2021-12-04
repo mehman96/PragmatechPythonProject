87521783e87234810c526dd9b2e220b8f03eec01
mehman96 <mirzeyevmehman000@gmail.com>
2021-12-04 11:06:48 +0400

    app static file update

--- E-commerce/myprojectdjango/urls.py
+++ E-commerce/myprojectdjango/urls.py
@@ -7,11 +7,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',include("home.urls",namespace="home"),),
-    path('product/',include("product.urls",namespace="product"),),
+    path('single/',include("product.urls",namespace="single"),),
     path('products/',include("product.urls",namespace="products"),),
     path('products/',include("product.urls",namespace="products1"),),
+    path('products/',include("product.urls",namespace="products2"),),
     path('about/',include("about.urls",namespace="about"),),
-    path('contact/',include("contact.urls",namespace="contact"),),
+    path('contact/',include("contact.urls",namespace="contact"),),   
     path('code/',include("pages.urls",namespace="code"),),
     path('icon/',include("pages.urls",namespace="icon"),),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- E-commerce/product/urls.py
+++ E-commerce/product/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 # yazdigimiz functions imporr edirk
-from .views import product,products,products1
+from .views import single,products,products1,products2
 
 app_name="product"
 
 
 urlpatterns = [
-    path('' ,product, name='product'),
+    path('single/' ,single, name='single'),
     path('' ,products, name='products'),
     path('' ,products1, name='products1'),
-
+    path('' ,products2, name='products2'),
 ]
